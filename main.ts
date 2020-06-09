02f54dc9b174aa30feb7e929d33275f4ea67efef
Guilherme <guilhermevendra@gmail.com>
2020-06-08 23:06:23 -0300

    update oak

--- main.ts
+++ main.ts
@@ -2,7 +2,6 @@ import { PORT, HOST, app } from "./bootstrap.ts";
 import homeRouter from "./core/modules/home/routes/homeRoute.ts";
 import loginRouter from "./core/modules/auth/login/cms/routes/loginRoute.ts";
 
-app.use();
 app.use(homeRouter.routes());
 app.use(loginRouter.routes());
 
