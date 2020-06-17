93b5cfafcbf8de3d9568f5b8399753c8d017bbed
Guilherme <guilhermevendra@gmail.com>
2020-06-16 21:00:59 -0300

    Reference entity form view

--- core/modules/entity_reference/assets/js/entityReference.js
+++ core/modules/entity_reference/assets/js/entityReference.js
@@ -1,3 +1,39 @@
 $(document).ready(function () {
-  console.log("ready!");
+  let entityReference = $('.form-view-entity-reference');
+
+  if (entityReference && entityReference.length > 0) {
+    entityReference.each(function () {
+      let dataEntities = $(this).data('entities');
+      let entityContainer = $(this);
+
+      $.each(dataEntities, function (entity, types) {
+        $.each(types, async function (_, type) {
+          let entities = await getEntities(entity, type);
+
+          if (entities && entities.data) {
+            $.each(entities.data, function (_, data) {
+              console.log(data);
+              entityContainer.append(data.data.title);
+            });
+          }
+        });
+      });
+
+    });
+  }
+
+  async function getEntities(entity, type) {
+    let result = null;
+    await $.ajax({
+      url: '/entity-reference/' + entity + '/' + type,
+      type: 'get',
+      dataType: 'json',
+      async: true,
+      timeout: (2 * 1000),
+      success: function (data) {
+        result = data;
+      }
+    });
+    return result;
+  }
 });
