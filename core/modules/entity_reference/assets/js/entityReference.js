83a27ef8a2d460f12f4c5b629229e6588378d4ad
Guilherme <guilhermevendra@gmail.com>
2020-06-17 17:46:09 -0300

    entities template

--- core/modules/entity_reference/assets/js/entityReference.js
+++ core/modules/entity_reference/assets/js/entityReference.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  let entityReference = $('.form-view-entity-reference');
+  let entityReference = $('.entity-reference-entities');
 
   if (entityReference && entityReference.length > 0) {
     entityReference.each(function () {
@@ -7,19 +7,71 @@ $(document).ready(function () {
       let entityContainer = $(this);
 
       $.each(dataEntities, function (entity, types) {
+        entityContainer.append(
+          `<div class="mb-3 entity ${entity}">
+            <h4 class="text-capitalize">${entity.replace('_',' ')}</h4>
+          </div>`
+        );
+
         $.each(types, async function (_, type) {
+          entityContainer.find('.entity.' + entity + '').first().append(
+            `<div class="mb-3 type ${type}">
+              <h5 class="text-capitalize">${type.replace('_',' ')}</h5>
+              <div class="items clearfix"></div>
+            </div>`
+          );
           let entities = await getEntities(entity, type);
 
           if (entities && entities.data) {
             $.each(entities.data, function (_, data) {
-              console.log(data);
-              entityContainer.append(data.data.title);
+              entityContainer.find('.type.' + type + ' > .items').first().append(
+                getTemplate(entity, data)
+              );
             });
           }
         });
       });
-
     });
+  }
+
+  function getTemplate(entity, data) {
+    let template;
+    switch (entity) {
+      case 'taxonomy':
+        template = getTaxonomyTemplate(data);
+        break;
+      case 'content':
+        template = getContentTemplate(data);
+        break;
+      default:
+        template = getDefaultTemplate(data);
+        break;
+    }
+    return template;
+  }
+
+  function getTaxonomyTemplate(data) {
+    let template = `
+      <div class="float-left text-center m-2 badge badge-primary p-2">
+        ${data.data.title}
+      </div>`;
+    return template;
+  }
+
+  function getContentTemplate(data) {
+    let template = `
+      <div class="float-left text-center m-2 badge badge-info p-2">
+        ${data.data.title}
+      </div>`;
+    return template;
+  }
+
+  function getDefaultTemplate(data) {
+    let template = `
+      <div class="float-left text-center m-2 badge badge-dark p-2">
+        ${data._id.$oid}
+      </div>`;
+    return template;
   }
 
   async function getEntities(entity, type) {
