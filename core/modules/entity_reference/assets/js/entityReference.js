23b6da39e2f6682e07a58b8542e82766977943f3
Guilherme <guilhermevendra@gmail.com>
2020-06-18 18:28:03 -0300

    entity reference

--- core/modules/entity_reference/assets/js/entityReference.js
+++ core/modules/entity_reference/assets/js/entityReference.js
@@ -1,76 +1,65 @@
 $(document).ready(function () {
-  let entityReference = $('.entity-reference-entities');
 
-  if (entityReference && entityReference.length > 0) {
-    entityReference.each(function (index) {
-      let dataEntities = $(this).data('entities');
-      let entityContainer = $(this);
+  buildReference();
 
-      $.each(dataEntities, function (entity, types) {
-        entityContainer.append(
-          `<div class="bg-light p-2 mb-3 entity ${entity}">
-            <a class="collapsed" href="#${entity}${index}" role="button" aria-controls="${entity}${index}" data-toggle="collapse" aria-expanded="false">
-              <h5 class="border-bottom border-white pb-2 text-center text-capitalize font-weight-bold">
-                ${entity.replace('_', ' ')}
-              </h5>
-            </a>
-            <div id="${entity}${index}" class="items collapse"></div>
-          </div>`
-        );
+  function buildReference() {
+    let entityReference = $('.entity-reference-entities');
+    if (entityReference && entityReference.length > 0) {
+      entityReference.each(function (index) {
+        let dataEntities = $(this).data('entities');
+        let field = $(this).data('field');
+        let entityContainer = $(this);
 
-        let entityItems = entityContainer.find('.entity.' + entity + ' > .items').first();
-
-        $.each(types, async function (_, type) {
-          entityItems.append(
-            `<div class="border-bottom mb-2 mt-2 p-2 type ${type}">
-              <div class="entity-header d-flex justify-content-between">
-                <h6 class="text-capitalize font-weight-bold text-secondary">
-                  ${type.replace('_', ' ')}
-                </h6>
-                <div class="actions">
-                  <span class="add-new">
-                    <a target="_blank" class="btn btn-outline-secondary btn-sm" href="/admin/${entity.replace('_', '-')}/${type.replace('_', '-')}/add">+</a>
-                  </span>
-                  <span class="refresh">
-                    <a data-entity="${entity}" data-type="${type}" class="btn btn-outline-secondary btn-sm" href="#">↻</a>
-                  </span>
-                </div>
-              </div>
-              <div class="items clearfix"></div>
+        $.each(dataEntities, function (entity, types) {
+          entityContainer.append(
+            `<div class="bg-light p-2 mb-3 field-${field} entity ${entity}">
+              <a class="collapsed" href="#${entity}${index}" role="button" aria-controls="${entity}${index}" data-toggle="collapse" aria-expanded="false">
+                <h5 class="border-bottom border-white pb-2 text-center text-capitalize font-weight-bold">
+                  ${entity.replace('_', ' ')}
+                </h5>
+              </a>
+              <div id="${entity}${index}" class="items collapse"></div>
             </div>`
           );
 
-          let entities = await getEntities(entity, type);
+          let entityItems = entityContainer.find('.entity.' + entity + ' > .items').first();
 
-          if (entities && entities.data) {
-            let typeItems = entityContainer.find('.type.' + type + ' > .items').first();
-            $.each(entities.data, function (_, data) {
-              typeItems.append(
-                getTemplate(entity, data)
-              );
-            });
-          }
+          $.each(types, async function (_, type) {
+            entityItems.append(
+              `<div class="border-bottom mb-2 mt-2 p-2 type ${type}">
+                <div class="entity-header d-flex justify-content-between">
+                  <h6 class="text-capitalize font-weight-bold text-secondary">
+                    ${type.replace('_', ' ')}
+                  </h6>
+                  <div class="actions">
+                    <span class="add-new">
+                      <a target="_blank" class="btn btn-outline-secondary btn-sm" href="/admin/${entity.replace('_', '-')}/${type.replace('_', '-')}/add">+</a>
+                    </span>
+                    <span class="refresh">
+                      <a data-field="${field}" data-entity="${entity}" data-type="${type}" class="btn btn-outline-secondary btn-sm" href="#">↻</a>
+                    </span>
+                  </div>
+                </div>
+                <div class="items clearfix"></div>
+              </div>`
+            );
+
+            let entities = await getEntities(entity, type);
+
+            if (entities && entities.data) {
+              let typeItems = entityContainer.find('.type.' + type + ' > .items').first();
+              $.each(entities.data, function (_, data) {
+                typeItems.append(
+                  getTemplate(entity, data)
+                );
+                callAction(field, data._id.$oid);
+              });
+            }
+          });
         });
       });
-    });
-  }
-
-  $('.refresh > a').click(async function (e) {
-    e.preventDefault();
-    let entity = $(this).data('entity');
-    let type = $(this).data('type');
-    let entities = await getEntities(entity, type);
-
-    if (entities && entities.data) {
-      let itemsList = $('.entity.' + entity + ' .type.' + type + ' > .items');
-      itemsList.html('');
-      $.each(entities.data, function (_, data) {
-        itemsList.append(
-          getTemplate(entity, data)
-        );
-      });
     }
-  });
+  }
 
   function getTemplate(entity, data) {
     let template;
@@ -90,25 +79,28 @@ $(document).ready(function () {
 
   function getTaxonomyTemplate(data) {
     let template = `
-      <div class="font-weight-light float-left text-center m-2 badge badge-primary p-2">
+      <a data-id="${data._id.$oid}" class="entity-action ${data._id.$oid} btn btn-info btn-sm m-2" href="#" role="button">
         ${data.data.title}
-      </div>`;
+        <span class="">+</span>
+      </a>`;
     return template;
   }
 
   function getContentTemplate(data) {
     let template = `
-      <div class="font-weight-light float-left text-center m-2 badge badge-info p-2">
+      <a class="entity-action ${data._id.$oid} btn btn-primary btn-sm m-2" href="#" role="button">
         ${data.data.title}
-      </div>`;
+        <span class="">+</span>
+      </a>`;
     return template;
   }
 
   function getDefaultTemplate(data) {
     let template = `
-      <div class="float-left text-center m-2 badge badge-dark p-2">
+      <a class="entity-action ${data._id.$oid} btn btn-primary btn-sm m-2" href="#" role="button">
         ${data._id.$oid}
-      </div>`;
+        <span class="">+</span>
+      </a>`;
     return template;
   }
 
@@ -124,6 +116,33 @@ $(document).ready(function () {
         result = data;
       }
     });
+
     return result;
   }
+
+  function callAction(field, item) {
+    $(`.field-${field} .${item}`).click(function (e) {
+      e.preventDefault();
+      console.log(item);
+    });
+  }
+
+  $('.refresh > a').click(async function (e) {
+    e.preventDefault();
+    let entity = $(this).data('entity');
+    let field = $(this).data('field');
+    let type = $(this).data('type');
+    let entities = await getEntities(entity, type);
+
+    if (entities && entities.data) {
+      let itemsList = $('.entity.' + entity + ' .type.' + type + ' > .items');
+      itemsList.html('');
+      $.each(entities.data, function (_, data) {
+        itemsList.append(
+          getTemplate(entity, data)
+        );
+        callAction(field, data._id.$oid);
+      });
+    }
+  });
 });
