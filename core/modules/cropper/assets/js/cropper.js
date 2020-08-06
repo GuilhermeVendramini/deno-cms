579365b43a8579dc935e6f6d74b1381beed7ac65
Guilherme <guilhermevendra@gmail.com>
2020-08-06 19:08:56 -0300

    file cropper

--- core/modules/cropper/assets/js/cropper.js
+++ core/modules/cropper/assets/js/cropper.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
   let cropperType = cropperOptions.querySelector('li.active a')?.getAttribute('data-aspect-ratio');
   let cropperTypeValue;
   let cropper;
+  let cropAlert = document.getElementById("crop-alert");
 
   if (cropperField.value) {
     let prepareCropperValues = JSON.parse(cropperField.value);
@@ -31,8 +32,99 @@ document.addEventListener('DOMContentLoaded', function () {
     let newCropPreview = document.createElement("img");
     newCropPreview.setAttribute('src', image.src);
     cropPreview.append(newCropPreview);
+
+    let oldCropActionButton = document.getElementById("crop-action");
+    oldCropActionButton?.remove();
+    let cropActionButton = document.createElement('a');
+    cropActionButton.setAttribute('href', '#');
+    cropActionButton.setAttribute('id', 'crop-action');
+    cropActionButton.onclick = cropAction;
+    cropPreview.append(cropActionButton);
     cropper = new Cropper(newCropPreview, options);
+    updateCropButton();
   };
+
+  function comparePreviewCrop() {
+    let preview;
+    let crop;
+
+    if (
+      cropperValues[cropperType] &&
+      cropperValues[cropperType]['cropped'] &&
+      cropperValues[cropperType]['cropped']['data']
+    ) {
+      crop = cropperValues[cropperType]['cropped']['data'];
+    } else {
+      return 0;
+    }
+
+    if (
+      cropperValues[cropperType] &&
+      cropperValues[cropperType]['preview']
+    ) preview = cropperValues[cropperType]['preview'];
+
+    if (
+      crop.height !== preview.height ||
+      crop.width !== preview.width ||
+      crop.x !== preview.x ||
+      crop.y !== preview.y
+    ) return 1;
+
+    return 2;
+  }
+
+  async function cropAction(e) {
+    e.preventDefault();
+    let cropperCanvas = cropper.getCroppedCanvas({ width: 160, height: 90 });
+    cropperCanvas.toBlob(async function (blob) {
+      let file = new File([blob], "filename.png");
+      await uploadMedia(file);
+      updateCropButton();
+    });
+  }
+
+  async function uploadMedia(file) {
+    let form = new FormData();
+    form.append(`media`, file);
+    let result = await fetch('/media/image', {
+      method: 'POST',
+      body: form,
+    }).then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+
+      return false;
+    });
+
+    if (result) {
+      let file = Object.values(result)[0];
+
+      if ("url" in file) {
+        let preview = cropper.getData();
+        if (
+          cropperValues[cropperType] &&
+          cropperValues[cropperType]['cropped']
+        ) {
+          cropperValues[cropperType]['cropped'] = result;
+        } else {
+          cropperValues[cropperType] = { cropped: result };
+        }
+
+        cropperValues[cropperType]['cropped']['data'] = preview;
+        cropperValues[cropperType]['preview'] = preview;
+
+
+        let cropperValuesStr = JSON.stringify(cropperValues);
+        cropperField.value = cropperValuesStr;
+      }
+    } else {
+      cropAlert.classList.remove('d-none');
+      cropAlert.innerHTML = 'Error uploading cropped image.';
+    }
+
+    return result;
+  }
 
   let config = { childList: true };
   let observer = new MutationObserver(callback);
@@ -50,22 +142,75 @@ document.addEventListener('DOMContentLoaded', function () {
     active?.classList.remove("active");
     this.parentNode.className += ' active';
     cropperType = this.getAttribute('data-aspect-ratio');
-    cropperTypeValue = cropperValues[cropperType];
+    if (
+      cropperValues[cropperType] &&
+      cropperValues[cropperType]['preview']
+    ) cropperTypeValue = cropperValues[cropperType];
     cropper.reset();
     cropper.setAspectRatio(eval(cropperType));
 
-    if (cropperTypeValue) cropper.setData(cropperTypeValue);
+    if (cropperTypeValue) cropper.setData(cropperTypeValue['preview']);
+
+    updateCropButton();
   }
 
   function cropReady() {
-    if (cropperValues) cropperTypeValue = cropperValues[cropperType];
-    if (cropperTypeValue) cropper.setData(cropperTypeValue);
+    if (
+      cropperValues[cropperType] &&
+      cropperValues[cropperType]['preview']
+    ) cropperTypeValue = cropperValues[cropperType];
+    if (cropperTypeValue) cropper.setData(cropperTypeValue['preview']);
+  }
+
+  function updateCropButton() {
+    let cropButton = document.getElementById("crop-action");
+    let previewCrop = comparePreviewCrop();
+    let cropButtonText = "Create crop";
+    let cropButtonClass = "mt-2 btn btn-secondary";
+    let oldResetCropButton = document.getElementById("reset-crop");
+    oldResetCropButton?.remove();
+
+    switch (previewCrop) {
+      case 1:
+        cropButtonText = "Save change";
+        cropButtonClass = "mt-2 btn btn-warning";
+
+        let resetCropButton = document.createElement('a');
+        resetCropButton.setAttribute('href', '#');
+        resetCropButton.setAttribute('id', 'reset-crop');
+        resetCropButton.className = "ml-2 mt-2 btn btn-info";
+        resetCropButton.innerHTML = "Reset";
+        resetCropButton.onclick = cropResetAction;
+        cropPreview.append(resetCropButton);
+        break;
+      case 2:
+        cropButtonText = "Saved";
+        cropButtonClass = "mt-2 btn btn-success";
+        break;
+    }
+
+    cropButton.className = cropButtonClass;
+    cropButton.innerHTML = cropButtonText;
+  }
+
+  function cropResetAction(e) {
+    e.preventDefault();
+    cropper.setData(cropperValues[cropperType]['cropped']['data']);
   }
 
   function cropend() {
     let cropData = cropper.getData();
-    cropperValues[cropperType] = cropData;
+    if (
+      cropperValues[cropperType] &&
+      cropperValues[cropperType]['preview']
+    ) {
+      cropperValues[cropperType]['preview'] = cropData;
+    } else {
+      cropperValues[cropperType] = { preview: cropData };
+    }
+
     let cropperValuesStr = JSON.stringify(cropperValues);
     cropperField.value = cropperValuesStr;
+    updateCropButton();
   }
 });
